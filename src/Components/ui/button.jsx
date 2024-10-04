267f6f49cf8f9ffc249e91c5ff03b74eff85ccfb
Mohammed Owais <filledstackdeveloper@gmail.com>
2024-10-04 17:40:56 +0530

    changes

--- src/Components/ui/button.jsx
+++ src/Components/ui/button.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
 
-import { cn } from '../../lib/utils';  // Adjust path based on your project structure
+import { cn } from './utils';  // Adjust path based on your project structure
 
 
 const buttonVariants = cva(
